d92a893388c1aa094787741e2b4b97b8f2100ff7
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-07-26 22:16:03 +0200

    Notnull has been added to userDto, so a User can not sign up without email.

--- microservices/composite-service/src/main/java/com/emrecan/workoutplanner/microservices/composite_service/dtos/UserDto.java
+++ microservices/composite-service/src/main/java/com/emrecan/workoutplanner/microservices/composite_service/dtos/UserDto.java
@@ -1,11 +1,15 @@
 package com.emrecan.workoutplanner.microservices.composite_service.dtos;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class UserDto {
+    @NonNull
     private String username;
+    @NonNull
     private String password;
+    @NonNull
     private String email;
     private String firstName;
     private String lastName;
